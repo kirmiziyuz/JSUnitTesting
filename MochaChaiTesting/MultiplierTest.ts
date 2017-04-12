84c9182798824445df3af9473296394e8fcad896
ekirmiziyuz <enginkirmiziyuz@gmail.com>
2017-04-12 14:39:31 +0200

    Cleanup master

--- MochaChaiTesting/MultiplierTest.ts
+++ MochaChaiTesting/MultiplierTest.ts
@@ -1,12 +1,1 @@
 ﻿import Multiplier from './Multiplier';
-import 'mocha';
-import { expect } from 'chai';
-
-describe('Multiplier function', () => {
-  it('should return 6', () => {
-    var multiplier = new Multiplier();
-    var result = multiplier.multiply(2, 3);
-
-    expect(result).to.equal(6);
-  });
-}); 
