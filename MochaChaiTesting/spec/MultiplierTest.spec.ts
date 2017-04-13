380be40da92e59dda748d41a5c5a9817bc164111
ekirmiziyuz <enginkirmiziyuz@gmail.com>
2017-04-13 14:59:16 +0200

    Init Jasmine, created Multiplier.spec.ts

---
+++ MochaChaiTesting/spec/MultiplierTest.spec.ts
@@ -0,0 +1,14 @@
+﻿import Multiplier from '../Multiplier';
+import 'jasmine';
+
+describe('Multiplier', () => {
+    var multiplier = new Multiplier();
+
+    it('should multiply', () => {
+        expect(multiplier.multiply(2, 3)).toEqual(6);
+    });
+
+    it('should fail', () => {
+        expect(1).toEqual(2);
+    });
+});
