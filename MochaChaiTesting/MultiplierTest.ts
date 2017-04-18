a5e488b91e0efaa370b39039095400215a036491
ekirmiziyuz <enginkirmiziyuz@gmail.com>
2017-04-18 09:13:35 +0200

    Expanded assertions to test behaviour of Mocha en Chai

--- MochaChaiTesting/MultiplierTest.ts
+++ MochaChaiTesting/MultiplierTest.ts
@@ -3,10 +3,27 @@ import 'mocha';
 import { expect } from 'chai';
 
 describe('Multiplier function', () => {
-  it('should return 6', () => {
-    var multiplier = new Multiplier();
-    var result = multiplier.multiply(2, 3);
+  var multiplier = new Multiplier();
+  var result = multiplier.multiply(2, 3);
 
+  it('should return 6', () => {
     expect(result).to.equal(6);
   });
+
+  it('should compare 6 to a string', () => {
+    expect(result).not.to.equal("string");
+  });
+
+  it('should compare 6 to a string', () => {
+    expect(result).not.to.be.a('string');
+  });
+
+  it('should compare 6 to "6"', () => {
+    expect(result).not.to.equal("6");
+  });
+
+  it('should expect 6 to be a number', () => {
+    expect(result).to.be.a('number');
+  });
+
 }); 
