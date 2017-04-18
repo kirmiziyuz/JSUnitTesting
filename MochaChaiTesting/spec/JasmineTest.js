b0cbb5f260de513644890de63a55c688e4badb79
ekirmiziyuz <enginkirmiziyuz@gmail.com>
2017-04-18 16:31:38 +0200

    Refactored example test to include failing assertion (for research purposes)

---
+++ MochaChaiTesting/spec/JasmineTest.js
@@ -0,0 +1,5 @@
+describe("Test", function () {
+  it('should fail when expecting 1 to be a string', function () {
+    expect(1).toEqual("string");
+  });
+});
--- MochaChaiTesting/spec/MultiplierTest.spec.ts
+++ MochaChaiTesting/spec/MultiplierTest.spec.ts
@@ -4,11 +4,7 @@ import 'jasmine';
 describe('Multiplier', () => {
     var multiplier = new Multiplier();
 
-    it('should multiply', () => {
+    it('should multiply 2 by 3 and return 6', () => {
         expect(multiplier.multiply(2, 3)).toEqual(6);
-    });
-
-    it('should fail', () => {
-        expect(1).toEqual(2);
     });
 });
