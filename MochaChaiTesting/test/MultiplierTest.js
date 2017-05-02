4127bf3a8d7ff9b131242216a2c14314844b1062
ekirmiziyuz <enginkirmiziyuz@gmail.com>
2017-05-02 15:09:11 +0200

    Added js files to test QUnit in VS2017

---
+++ MochaChaiTesting/test/MultiplierTest.js
@@ -0,0 +1,6 @@
+﻿
+QUnit.test("MultiplierTest", function (assert) {
+  var multiplier = require("../Multiplier");
+  assert.deepEqual(multiplier.multiply(2, 3), 6, "top");
+  assert.deepEqual(multiplier.multiply(2, 3), "zes", "niet top");
+});
---
+++ MochaChaiTesting/test/MultiplierTest.ts
@@ -0,0 +1,8 @@
+﻿import Multiplier from '../Multiplier';
+import 'qunit';
+
+QUnit.test("MultiplierTest", (assert) => {
+    var multiplier = new Multiplier();
+    assert.deepEqual(multiplier.multiply(2, 3), 6, "top");
+    assert.deepEqual(multiplier.multiply(2, 3), "zes", "niet top");
+});
